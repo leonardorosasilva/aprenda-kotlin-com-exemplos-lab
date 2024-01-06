8d2808873366443938ed063901920a583bb89fef
Leonardo Rosa da Silva <03466316022@edu.udesc.br>
2024-01-05 23:12:04 -0300

    .

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,32 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val id: Int, val nome: String) 
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+    println("Inscritos: $inscritos")
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+        inscritos.add(usuario)
+        println("Inscritos: $inscritos")
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val usuario = Usuario(1, "João")
+    val conteudo = ConteudoEducacional("Kotlin", 60)
+    val formacao = Formacao("Android", listOf(conteudo))
+
+    formacao.matricular(usuario)
+
+    println("Formação: $formacao")
+
 }
